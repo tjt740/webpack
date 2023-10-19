60e6fcb24bb104b3109c4f05297d63622f3ca603
t <15751666937@163.com>
2023-10-20 00:18:23 +0800

    111

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-var fn = function (params) {
-    var number = 1;
-    var result = Number(params)
-        ? number + Number(params)
-        : String(number).concat(String(params));
-    return result;
-};
-fn(1);
-fn('123');
-console.log(fn('你好'));
-function component() {
+async function getComponent() {
     const element = document.createElement('div');
-  
-    // lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    const { default: _ } = await import('lodash');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
     return element;
-  }
+}
 
-  document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-       entry: './src/app.js',
-       output: {
-           path: path.resolve(__dirname, 'dist'),
-           filename: 'main.js'
-       },
-       mode: 'development' // 设置mode
-   }
+    entry: {
+        main: './src/index.js',
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            progress: true, // 在浏览器中以百分比显示编译进度。
+            overlay: true, // 当出现编译错误或警告时，在浏览器中显示全屏覆盖。
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }), // Generates default index.html
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+};
