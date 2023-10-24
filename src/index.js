f42520df43831f981a4ad8c18f78939a8b4c76e3
t <15751666937@163.com>
2023-10-25 00:28:26 +0800

    1111

--- src/index.js
+++ src/index.js
@@ -1,70 +1,26 @@
-'use strict';
-var __extends =
-    (this && this.__extends) ||
-    (function () {
-        var extendStatics = function (d, b) {
-            extendStatics =
-                Object.setPrototypeOf ||
-                ({ __proto__: [] } instanceof Array &&
-                    function (d, b) {
-                        d.__proto__ = b;
-                    }) ||
-                function (d, b) {
-                    for (var p in b)
-                        if (Object.prototype.hasOwnProperty.call(b, p))
-                            d[p] = b[p];
-                };
-            return extendStatics(d, b);
-        };
-        return function (d, b) {
-            if (typeof b !== 'function' && b !== null)
-                throw new TypeError(
-                    'Class extends value ' +
-                        String(b) +
-                        ' is not a constructor or null'
-                );
-            extendStatics(d, b);
-            function __() {
-                this.constructor = d;
-            }
-            d.prototype =
-                b === null
-                    ? Object.create(b)
-                    : ((__.prototype = b.prototype), new __());
-        };
-    })();
-Object.defineProperty(exports, '__esModule', { value: true });
-var main_1 = require('./main');
-var Aa = /** @class */ (function () {
-    function Aa() {
-        // private tall: number;
-        this.weight = 130;
-        this.name = 'tjt';
-        this.age = 123;
-    }
-    Aa.prototype.fn = function (params) {
-        if (params === void 0) {
-            params = '1234';
-        }
-        return params;
-    };
-    return Aa;
-})();
-var Bb = /** @class */ (function (_super) {
-    __extends(Bb, _super);
-    function Bb(weight) {
-        var _this = this;
-        var element = document.createElement('pre');
-        element.innerHTML = [
-            'Hello webpack!',
-            '5 cubed is equal to ' + (0, main_1.cube)(5),
-        ].join('\n\n');
-        _this = _super.call(this) || this;
-        console.log(_this.weight, weight);
-        return _this;
-    }
-    return Bb;
-})(Aa);
-var b = new Bb(1);
-var b1 = new Bb(120);
-console.log(process.env.NODE_ENV);
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+
+    const button = document.createElement('button');
+    const br = document.createElement('br');
+
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    // Note that because a network request is involved, some indication
+    // of loading would need to be shown in a production-level site/app.
+    button.onclick = (e) =>
+        import(/* webpackChunkName: "print" */ './print').then((module) => {
+            const print = module.default;
+
+            print();
+        });
+
+    return element;
+}
+
+document.body.appendChild(component());
